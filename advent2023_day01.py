57399b77c81dbd8877e9b6ae1ee32118e4023837
Mikko Heikela <mikko.heikela@gmail.com>
2024-01-20 00:51:49 +0000

    Solve 2023 day 1 in Python

---
+++ advent2023_day01.py
@@ -0,0 +1,49 @@
+import common.inputs
+from collections import ChainMap
+from typing import Dict
+
+digits = {str(i) : i for i in range(10)}
+
+spelled_digits = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four' : 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
+
+all_digits = ChainMap(digits, spelled_digits)
+
+def contains_at_pos(line: str, pos: int, sub_string: str) -> bool:
+    return line[pos:].startswith(sub_string)
+
+def first_digit(line: str, digits: Dict[str, int]) -> int:
+    for i in range(len(line)):
+        for digit_representation, value in digits.items():
+            if contains_at_pos(line, i, digit_representation):
+                return value
+    raise ValueError("No digit found in line")
+
+def last_digit(line: str, digits: Dict[str, int]) -> int:
+    for i in range(len(line) - 1, -1, -1):
+        for digit_representation, value in digits.items():
+            if contains_at_pos(line, i, digit_representation):
+                return value
+    raise ValueError("No digit found in line")
+
+def line_to_number(line: str, digits: Dict[str, int]) -> int:
+    return 10 * first_digit(line, digits) + last_digit(line, digits)
+
+def main():
+    input = common.inputs.get_input(2023, 1)
+    part1 = sum(line_to_number(line, digits) for line in input)
+    print("part 1: %d" % part1)
+    part2 = sum(line_to_number(line, all_digits) for line in input)
+    print("part 2: %d" % part2)
+
+if __name__ == '__main__':
+    main()
---
+++ common/inputs.py
@@ -0,0 +1,28 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+def get_input(year: int, day: int) -> str:
+    """Get the input for a given year and day.
+    
+    Uses a predownloaded input file if available, otherwise downloads the input.
+    """
+    if not os.path.exists(f"inputs/{year}/{day}.txt"):
+        # Download the input
+        load_dotenv()
+        session = requests.Session()
+        cookie_content = os.getenv("SESSION_COOKIE")
+        if cookie_content is None:
+            raise ValueError("No session cookie found")
+        session.cookies.set("session", os.getenv("SESSION_COOKIE"))
+        response = session.get(f"https://adventofcode.com/{year}/day/{day}/input")
+        if response.status_code != 200:
+            print(response.text)
+            print(response.status_code)
+            print(response.headers)
+            raise ValueError(f"Failed to download input for year {year} day {day}")
+        os.makedirs(f"inputs/{year}", exist_ok=True)
+        with open(f"inputs/{year}/{day}.txt", "w") as f:
+            f.write(response.text)
+    with open(f"inputs/{year}/{day}.txt") as f:
+        return f.readlines()
