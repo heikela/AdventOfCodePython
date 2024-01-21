6368e35468cf08369ee4e7c9c18471619675396d
Mikko Heikela <mikko.heikela@gmail.com>
2024-01-21 17:44:41 +0000

    Download day 3 input and a relevant example

---
+++ advent2023_day03.py
@@ -0,0 +1,10 @@
+from common.inputs import get_input, get_test_snippet
+
+def main():
+    input = get_input(2023, 3)
+    print(input)
+    snippet = get_test_snippet(2023, 3, 0)
+    print(snippet)
+
+if __name__ == '__main__':
+    main()
